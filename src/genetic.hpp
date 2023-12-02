563b3012e0ce8da1d5acee16a020ef682c7ac86f
Aenariss <aenariss@gmail.com>
2023-12-02 10:10:31 +0100

    edit GA header file, remove test python implementation

--- src/genetic.hpp
+++ src/genetic.hpp
@@ -8,6 +8,7 @@
 #include <random>
 
 void genetic();
+void print2D(vector<vector<int>> vec);
 
 double distance(Node first, Node second);
 double fitness(vector<int> solution, VRPDataReader reader, vector<vector<double>> distanceMatrix);
@@ -16,5 +17,12 @@ double calculateCustomerDistance(vector<int> current_route, vector<vector<double
 vector<vector<int>> initPopulation(vector<Node> customers, size_t populationSize);
 vector<vector<double>> calculateDistanceMatrix(vector<Node> customers);
 vector<int> binaryTournament(vector<vector<int>> population, VRPDataReader reader, vector<vector<double>> distanceMatrix);
+vector<vector<int>> getRoutes(vector<int> solution, VRPDataReader reader, vector<vector<double>> distanceMatrix);
+vector<int> mutation(vector<int> solution, VRPDataReader reader, vector<vector<double>> distanceMatrix);
+vector<int> binaryTournament(vector<vector<int>> population, VRPDataReader reader, vector<vector<double>> distanceMatrix);
+vector<vector<int>> removeMember(vector<vector<int>> population, vector<int> to_remove);
+
+pair<vector<int>, vector<int>> orderedCrossover(vector<int> parent1, vector<int> parent2);
+pair<pair<double,double>, pair<vector<int>, vector<int>>> populationFitness(vector<vector<int>> population, VRPDataReader reader, vector<vector<double>> distanceMatrix);
 
 #endif
