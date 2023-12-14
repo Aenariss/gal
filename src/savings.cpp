ffc88c04b912b9211d3346e47b8e1792d07b8d28
PeterMocary <petermocary@gmail.com>
2023-12-14 15:39:12 +0100

    fixed indexing in savings and added another metric to the output

--- src/savings.cpp
+++ src/savings.cpp
@@ -19,8 +19,16 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
     int maxRoutesCnt = ceil((int) nodes.size()/2);
     multiset<Savings, SavingsRanker> savings;
     vector<bool> customersServed(nodes.size() - 1, false); // served status (excluding the depot)
-
+    
     // Complexity - m (number of edges) and n (number of nodes)
+    // m = 1/2 * n(n-1) ~= n^2
+
+    // Space Complexity - 
+    // O(m+n+n)
+    // O(n^2+n+n)
+    // O(n^2) - quadratic
+    
+    // Time Complexity 
     // O(m + m*log(n) + (m*n + m*(n+m) + n)*n)
     // O(n^2 + n^2*log(n) + (n^3 + n^2*(n+n^2) + n)*n)
     // O(n^2 + n^2*log(n) + (n^3 + n^3 + n^4 + n)*n)
@@ -34,11 +42,11 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
 
     // Step two:
     // Calculate all savings between every two customers. Rank the savings and omit those below zero.
-    // O(m)
-    for (int i = 0; i < (int)nodes.size()-1; i++) {
+    // time - O(m)
+    for (int i = 2; i < (int)nodes.size()-1; i++) {
         for (int j = i + 1; j < (int)nodes.size()-1; j++) {
-            // Note: customer IDs always start at 1
-            Savings newSavings(i+1, j+1, distanceMatrix);
+            // Note: customer IDs always start at 2
+            Savings newSavings(i, j, distanceMatrix);
             if (newSavings.value < .0) {
                 continue;
             }
@@ -53,17 +61,17 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
         // Choose two customers with maximum savings satisfied the truck load limit as the initial route.
         Route route(vehicleCapacity);
         routesCnt++;
-        // O(m)
+        // time - O(m)
         for (auto& candidateSavings: savings) {
-            if (customersServed[candidateSavings.customerOneId-1] or customersServed[candidateSavings.customerTwoId-1]){
+            if (customersServed[candidateSavings.customerOneId-2] or customersServed[candidateSavings.customerTwoId-2]){
                 continue;
             }
             // O(n)
             bool addedToRoute = route.appendCustomersIfCapacity(candidateSavings.customerOneId, candidateSavings.customerTwoId,
                                                                 requests, distanceMatrix);
             if (addedToRoute) {
-                customersServed[candidateSavings.customerOneId-1] = true;
-                customersServed[candidateSavings.customerTwoId-1] = true;
+                customersServed[candidateSavings.customerOneId-2] = true;
+                customersServed[candidateSavings.customerTwoId-2] = true;
                 break;
             }
         }
@@ -71,7 +79,7 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
         vector<Savings> toDelete;
         // O(m)
         for (auto& candidateNextSavings: savings) {
-            if (customersServed[candidateNextSavings.customerOneId-1] and customersServed[candidateNextSavings.customerTwoId-1]) {
+            if (customersServed[candidateNextSavings.customerOneId-2] and customersServed[candidateNextSavings.customerTwoId-2]) {
                 continue;
             }
             // Step four:
@@ -90,20 +98,20 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
                 candidateCustomerId = candidateNextSavings.customerOneId;
             }
 
-            if (candidateCustomerId != -1 and !customersServed[candidateCustomerId-1]) { // A candidate that can be added to the start or end of route was found
+            if (candidateCustomerId != -1 and !customersServed[candidateCustomerId-2]) { // A candidate that can be added to the start or end of route was found
                 // 4.2)
-                // O(m)
+                // time - O(m)
                 // NOTE: if the step of finding max savings is omitted entirely the algorithm seems to be giving better
                 // results. A single implementation of this algorithm was found on github and it also omits this step.
                 // However, the step was preserved to compare stick with definition of algorithm from the paper.
                 Savings maxSavings = getMaxSavings(savings, candidateCustomerId, customersServed);
                 if (candidateNextSavings == maxSavings) {
-                    //O(n)
+                    // time - O(n)
                     bool added = route.addCustomerIfCapacity(candidateCustomerId,
                                                              requests[candidateCustomerId-2].quantity, start,
                                                              distanceMatrix);
                     if (added) {
-                        customersServed[candidateCustomerId-1] = true;
+                        customersServed[candidateCustomerId-2] = true;
                     }
                 }
             }
@@ -134,12 +142,15 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
 
     double totalDistance = 0;
     int routeNum = 0;
-    int routesWithTwoNodes = 0;
+    int routesWithOneNode = 0;
+    int routesWithTwoNodes= 0;
     double unusedCapacity = .0;
     for (auto& route : routes) {
         routeNum++;
         totalDistance += route.distance;
-        if (route.getSize() == 2) {
+        if (route.getSize() == 1) {
+            routesWithOneNode++;
+        } else if (route.getSize() == 2) {
             routesWithTwoNodes++;
         }
         unusedCapacity = route.vehicleCapacity - route.currentQuantity;
@@ -149,6 +160,7 @@ void savingsAlgorithm(const vector<Node>& nodes, const vector<Request>& requests
     cout << "Overall distances " << totalDistance << endl;
     cout << "Vehicles " << routeNum << endl;
     cout << "Average number of customers " << nodes.size() / routeNum << endl;
+    cout << "Number of routes linking only one customer " << routesWithOneNode<< endl;
     cout << "Number of routes linking only two customers " << routesWithTwoNodes << endl;
     cout << "Unused capacity " << unusedCapacity << endl;
     cout << "Time of the algorithm " << algorithmDuration.count() << " microseconds" << endl;
@@ -158,7 +170,7 @@ void createRouteForNotServedCustomers(const vector<bool>& isServed, vector<Route
                                       const vector<Request>& requests, const vector<vector<double>>& distanceMatrix) {
     vector<int> idsNotInRouteYet;
     for (int i = 0; i < (int)isServed.size(); i++) {
-        if (!isServed[i]) idsNotInRouteYet.push_back(i+1);
+        if (!isServed[i]) idsNotInRouteYet.push_back(i+2);
     }
     // create routes for every customer that was not yet served
     for (auto& id : idsNotInRouteYet) {
@@ -171,7 +183,7 @@ void createRouteForNotServedCustomers(const vector<bool>& isServed, vector<Route
 Savings getMaxSavings(multiset<Savings, SavingsRanker>& savings, int customerId, const vector<bool>& isServed) {
     Savings maxSavings;
     for (auto& currentSavings : savings) {
-        if (isServed[currentSavings.customerOneId-1] and isServed[currentSavings.customerTwoId-1]) {
+        if (isServed[currentSavings.customerOneId-2] and isServed[currentSavings.customerTwoId-2]) {
             continue;
         }
         if (currentSavings.customerOneId == customerId or currentSavings.customerTwoId == customerId) {
@@ -185,9 +197,9 @@ Savings::Savings(int customerOneId, int customerTwoId, vector<vector<double>>& d
     this->customerOneId = customerOneId;
     this->customerTwoId = customerTwoId;
 
-    double distanceBetweenClients = distanceMatrix[customerOneId][customerTwoId];
-    double depotDistanceClientOne = distanceMatrix[0][customerOneId];
-    double depotDistanceClientTwo = distanceMatrix[0][customerTwoId];
+    double distanceBetweenClients = distanceMatrix[customerOneId-1][customerTwoId-1];
+    double depotDistanceClientOne = distanceMatrix[0][customerOneId-1];
+    double depotDistanceClientTwo = distanceMatrix[0][customerTwoId-1];
     this->value = depotDistanceClientOne + depotDistanceClientTwo - distanceBetweenClients;
 }
 
@@ -219,10 +231,10 @@ bool Route::addCustomerIfCapacity(int customerId, double requestedQuantity, bool
     if (requestedQuantity + currentQuantity <= vehicleCapacity) {
         currentQuantity += requestedQuantity;
         if (start) {
-            distance += distanceMatrix[customerId][this->getStart()];
+            distance += distanceMatrix[customerId-1][this->getStart()-1];
             route.push_front(customerId);
         } else {
-            distance += distanceMatrix[customerId][this->getEnd()];
+            distance += distanceMatrix[customerId-1][this->getEnd()-1];
             route.push_back(customerId);
         }
         return true;
@@ -237,7 +249,7 @@ bool Route::appendCustomersIfCapacity(int customerOneId, int customerTwoId, cons
         currentQuantity += requestedQuantity;
         route.push_back(customerOneId);
         route.push_back(customerTwoId);
-        distance += distanceMatrix[customerOneId][customerTwoId];
+        distance += distanceMatrix[customerOneId-1][customerTwoId-1];
         return true;
     }
     return false;
@@ -270,5 +282,5 @@ int Route::getSize() const {
 }
 
 void Route::addDistancesToDepot(const vector<vector<double>> &distanceMatrix) {
-    this->distance += distanceMatrix[0][this->getStart()] + distanceMatrix[0][this->getEnd()];
+    this->distance += distanceMatrix[0][this->getStart()-1] + distanceMatrix[0][this->getEnd()-1];
 }
